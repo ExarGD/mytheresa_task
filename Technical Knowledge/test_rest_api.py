8171ad4539c843e5a26a087aa0fd63f5160c0f99
Adel <exar@tmtm.ru>
2020-01-21 19:18:07 +0300

    REST API task done

--- Technical Knowledge/test_rest_api.py
+++ Technical Knowledge/test_rest_api.py
@@ -1,7 +1,22 @@
 import requests
 import json
 
-url = "https://www.food2fork.com/api/search?key=e676ea4152b2077f7e7bef634e232fff"
+search_request = "tetris+language:assembly"
+url = "https://api.github.com/search/repositories?q=" + search_request + "&sort=stars&order=desc"
 
-response = requests.post(url)
-print(response.status_code)
+response = requests.get(url)
+assert response.status_code == 200
+
+data = response.text
+
+assert "total_count" in data
+assert "incomplete_results" in data
+assert "items" in data
+
+print(response.json()["total_count"])
+print(len(response.json()["items"]))
+# should be equal to total_count if total_count <= 30, or equal to 30 if total_count > 30
+
+with open("response.json", "w") as f:
+  f.write(response.text)
+
